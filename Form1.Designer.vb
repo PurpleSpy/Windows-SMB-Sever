af1001675a1ada2ea7c82ec3e1acfcc548b9f9e6
PurpleSpy <36999286+PurpleSpy@users.noreply.github.com>
2025-12-16 23:00:19 -0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        Button1 = New Button()
+        Label1 = New Label()
+        RichTextBox1 = New RichTextBox()
+        CheckedListBox1 = New CheckedListBox()
+        Panel2 = New Panel()
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(Button1)
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(RichTextBox1)
+        Panel1.Controls.Add(CheckedListBox1)
+        Panel1.Location = New Point(4, 9)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(867, 419)
+        Panel1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Enabled = False
+        Button1.Location = New Point(19, 377)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(146, 23)
+        Button1.TabIndex = 3
+        Button1.Text = "Kill SMB Links"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(637, 381)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 15)
+        Label1.TabIndex = 2
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Location = New Point(19, 193)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.ReadOnly = True
+        RichTextBox1.Size = New Size(845, 165)
+        RichTextBox1.TabIndex = 1
+        RichTextBox1.Text = ""
+        ' 
+        ' CheckedListBox1
+        ' 
+        CheckedListBox1.CheckOnClick = True
+        CheckedListBox1.FormattingEnabled = True
+        CheckedListBox1.Location = New Point(21, 15)
+        CheckedListBox1.Name = "CheckedListBox1"
+        CheckedListBox1.Size = New Size(843, 166)
+        CheckedListBox1.TabIndex = 0
+        ' 
+        ' Panel2
+        ' 
+        Panel2.Controls.Add(Panel1)
+        Panel2.Enabled = False
+        Panel2.Location = New Point(8, 12)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(874, 439)
+        Panel2.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(894, 457)
+        Controls.Add(Panel2)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Name = "Form1"
+        Text = "SMB Link Killer"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,137 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = If(Security.Principal.WindowsPrincipal.Current.IsInRole("ADMINISTRATORS"), "ADMIN", "USER" & vbCrLf & "Please Restart as Admin to use program")
+        If Security.Principal.WindowsPrincipal.Current.IsInRole("ADMINISTRATORS") Then
+            Panel2.Enabled = True
+            initCheckbox()
+
+            Dim incheckup As New Timer
+            AddHandler incheckup.Tick, AddressOf autoupdate
+            incheckup.Interval = 30000
+            incheckup.Enabled = True
+
+        Else
+            writeText("Please restart program in admin mode to continue")
+        End If
+
+
+
+
+    End Sub
+
+
+
+    Sub initCheckbox()
+        CheckedListBox1.Enabled = False
+        CheckedListBox1.Items.Clear()
+
+        Dim startdetails As New ProcessStartInfo("powershell", "net share")
+        Dim pshell As New Process
+        pshell.StartInfo = startdetails
+        startdetails.CreateNoWindow = True
+        startdetails.RedirectStandardError = True
+        startdetails.RedirectStandardOutput = True
+        pshell.Start()
+
+        Dim detailofrun As String = pshell.StandardOutput.ReadToEnd
+
+        Dim searchPattern As New Text.RegularExpressions.Regex("^([a-z\$]*) *([a-z]+\:\\[a-z0-9]*|)", RegexOptions.IgnoreCase Or RegexOptions.Multiline)
+
+
+
+        For Each itm As Match In searchPattern.Matches(detailofrun)
+            If itm.Groups(1).Value.ToLower = "" Or itm.Groups(1).Value.ToLower = "share" Or itm.Groups(1).Value.ToLower = " " Or itm.Groups(0).Value.ToLower = "" Or itm.Groups(1).Value.ToLower = "the" Then
+                Continue For
+            End If
+
+            CheckedListBox1.Items.Add(itm.Groups(1).Value & vbTab & vbTab & vbTab & itm.Groups(2).Value)
+
+
+        Next
+
+
+
+        CheckedListBox1.Enabled = True
+
+        If CheckedListBox1.Items.Count > 0 Then
+            Button1.Enabled = True
+        Else
+            Button1.Enabled = False
+        End If
+    End Sub
+
+    Sub processKillLink()
+        Panel1.Enabled = False
+
+        writeText("Started killing links")
+
+        For Each itm As String In CheckedListBox1.CheckedItems
+            killSMBLink(itm.Split(vbTab)(0))
+
+        Next
+
+
+        writeText("Completed killing links")
+
+        initCheckbox()
+
+        Panel1.Enabled = True
+    End Sub
+
+    Sub killSMBLink(Smbshare)
+        Dim procstart As New ProcessStartInfo("powershell", New String() {"Remove-SmbShare", "-Name", """" & Smbshare & """", "-force"})
+        procstart.CreateNoWindow = True
+        procstart.RedirectStandardError = True
+        procstart.RedirectStandardOutput = True
+        procstart.RedirectStandardInput = True
+
+        Try
+
+            Dim cpr As New Process
+            cpr.StartInfo = procstart
+            cpr.Start()
+
+            Dim errorlines As String = cpr.StandardError.ReadToEnd
+
+            writeText("Removed SMB Share " & Smbshare & cpr.StandardOutput.ReadToEnd)
+
+            writeText("Did not remove SMB Share because " & Smbshare & errorlines)
+
+        Catch ex As Exception
+
+            writeText("Failed to remove " & Smbshare)
+
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        processKillLink()
+    End Sub
+
+    Sub autoupdate(sender As Object, e As EventArgs)
+        initCheckbox()
+    End Sub
+
+
+    Sub writeText(text As String)
+        RichTextBox1.Text += vbCrLf & text
+    End Sub
+
+    Private Sub CheckedListBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles CheckedListBox1.MouseDown
+        Dim therewasacheckbox As Boolean = (CheckedListBox1.CheckedItems.Count > 0)
+
+        If e.Button = MouseButtons.Right Then
+
+            For i As Integer = 0 To CheckedListBox1.Items.Count - 1
+
+                CheckedListBox1.SetItemChecked(i, Not therewasacheckbox)
+            Next
+
+        End If
+
+
+    End Sub
+End Class
+
